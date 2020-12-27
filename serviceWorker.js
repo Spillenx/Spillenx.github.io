f4f176ed478846278a4403859ad92f8c5646e6f0
spillenx <spillenx@gmail.com>
2020-12-27 22:59:33 +0100

    New version

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,56 +17,66 @@ const filesToCache = [
 ]
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-    .then((cache) => {
-      return cache.addAll(filesToCache)
-    })
-    // .then(() => {
-    //   self.skipWaiting()
-    // })
-  )
+  event.waitUntil(caches.open(cacheName)
+  .then(cache => {
+    return cache.addAll(filesToCache)
+  }))
 })
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-    .then((cacheName) => {
-      cacheName.forEach((value) => {
-        if(value.indexOf(VERSION) < 0) {
-          caches.delete(value)
-        }
-      })
-      return
-    })
-  )
-})
-
-self.addEventListener('activate', function (event) { 
-  event.waitUntil( 
-    caches.keys()
-        .then(function (cacheNames) { 
-              cacheNames.forEach(function (value) { 
-              if (value.indexOf(VERSION) < 0) { 
-                  caches.delete(value); 
-              } 
-          }); 
-          return; 
-      }) 
-  );  
-}); 
-
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+ 
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response
-        }
-        return fetch(event.request)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   )
-})
+ })
+
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(cacheName)
+//     .then((cache) => {
+//       return cache.addAll(filesToCache)
+//     })
+//     // .then(() => {
+//     //   self.skipWaiting()
+//     // })
+//   )
+// })
+
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys()
+//     .then(cacheName => {
+//       cacheName.forEach((value) => {
+//         if(value.indexOf(VERSION) < 0) {
+//           caches.delete(value)
+//         }
+//       })
+//       return
+//     })
+//   )
+// })
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(fetch(event.request)
+//   .then((response) => {
+//     return response || caches.match(event.request)
+//   }))
+// })
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         if(response) {
+//           return response
+//         }
+//         return fetch(event.request)
+//       })
+//       .catch(error => {
+//         console.log(error)
+//       })
+//   )
+// })
